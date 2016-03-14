0fb6c777ba03d4576d57a563c13fe7bc130fc6e1
HomePC <ira.intertech@gmail.com>
2016-03-14 17:47:56 +0200

    blog gallery

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
 
-    jQuery('.slider').bxSlider();
+
 
     jQuery(window).load(function() {
         gutter_in_pixels = jQuery(".item-project").width() * 0.02;
@@ -10,16 +10,21 @@ jQuery(document).ready(function() {
             resizeable: true,
             gutter: gutter_in_pixels
         });
+        jQuery('.slider').bxSlider({
+            auto: true
+        });
 
 
-        //filter project
+
 
 
 
 
     });
 
-    var posts = jQuery('.item-project');
+
+    /*var posts = jQuery('.item-project');
+
     jQuery(".project-nav li" ).click(  function() {
 
         var nav_active = jQuery(this);
@@ -30,44 +35,86 @@ jQuery(document).ready(function() {
         jQuery(nav_active).addClass("active");
 
         var customType = jQuery(this).data('filter'); // category
-
+        console.log(customType);
         posts
             .css("opacity", 0.7)
             .filter(function () {
-                return jQuery(this).data('cat') === customType;
+                if(customType=="all"){
+                    posts.css("opacity", 1);
+                }else {
+                    return jQuery(this).data('cat') === customType;
+                }
+
+                //return jQuery(this).data('cat') === customType;
             })
             .css("opacity", 1);
 
 
 
-    });
-
-
-
-
-    /*jQuery(window).scroll(function () {
-
-        if ((jQuery(this).scrollTop() > 10) && (jQuery(window).width() > 1199)) {
-            jQuery("#header").addClass('fixed');
-
-
-        } else {
-            jQuery("#header").removeClass('fixed');
-
-        }
     });*/
 
+    jQuery(".project-nav li").click(function(){
+        var nav_active = jQuery(this);
+        console.log(nav_active);
+        jQuery('.project-nav li').each(function(){
+            jQuery(this).removeClass("active");
+        });
+        jQuery(nav_active).addClass("active");
 
-    jQuery("#menu").on("click","a[href^='#']", function (event) {
+
+        var customType = jQuery(this).attr('data-cat');
+        console.log(customType);
+        if(customType==="all") {
+            jQuery('.item-project').css("opacity", 1);
+            return;
+        }else{
+            jQuery('.item-project').each(function(){
+                if(jQuery(this).attr('data-cat').indexOf(customType) < 0) jQuery(this).css("opacity", 0.7); else jQuery(this).css("opacity", 1);
+            });
+        }
+
+
+    });
+    jQuery(window).resize(function() {
+
+    });
+
+
+
+
+
+    jQuery(document).on("click","a[href^='#']", function (event) {
         event.preventDefault();
-        //var id  = jQuery(this).attr('href'), top = jQuery(id).offset().top;
         jQuery('html, body').animate({ scrollTop:  jQuery('div[id="'+this.hash.slice(1)+'"]').offset().top }, 1000 );
 
-        //jQuery('body,html').animate({scrollTop: top}, 1500);
     });
+
 
 
 });
+
+
+window.onload = function(){
+    jQuery('.blog-img').each(function () {
+        this.onclick = function (event) {
+            event = event || window.event;
+            var target = event.target || event.srcElement,
+                link = target.src ? target.parentNode : target,
+                options = {index: link, event: event},
+                links = this.getElementsByTagName('a');
+            blueimp.Gallery(links, options);
+        };
+    });
+
+    /*document.getElementById('blog-img').onclick = function (event) {
+        event = event || window.event;
+        var target = event.target || event.srcElement,
+            link = target.src ? target.parentNode : target,
+            options = {index: link, event: event},
+            links = this.getElementsByTagName('a');
+        blueimp.Gallery(links, options);
+    };*/
+};
 
 
 
